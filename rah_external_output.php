ef553a3213f907870a8ac2da3a35d5db3721a926
Jukka Svahn <web@rahinaa.biz>
2012-12-21 23:36:35 +0200

    Headers.

--- rah_external_output.php
+++ rah_external_output.php
@@ -3,12 +3,12 @@
 /**
  * Rah_external_output plugin for Textpattern CMS
  *
- * @author Jukka Svahn
- * @date 2009-
+ * @author  Jukka Svahn
+ * @date    2009-
  * @license GNU GPLv2
- * @link http://rahforum.biz/plugins/rah_external_output
+ * @link    http://rahforum.biz/plugins/rah_external_output
  *
- * Copyright (C) 2012 Jukka Svahn <http://rahforum.biz>
+ * Copyright (C) 2012 Jukka Svahn http://rahforum.biz
  * Licensed under GNU Genral Public License version 2
  * http://www.gnu.org/licenses/gpl-2.0.html
  */
