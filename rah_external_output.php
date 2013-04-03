8c16c44c834a4f8ef5f841b68d1452400b42c44d
Jukka Svahn <web@rahinaa.biz>
2013-04-03 11:25:39 +0300

    Bump the date.

--- rah_external_output.php
+++ rah_external_output.php
@@ -8,7 +8,7 @@
  * @license GNU GPLv2
  * @link    http://rahforum.biz/plugins/rah_external_output
  *
- * Copyright (C) 2012 Jukka Svahn http://rahforum.biz
+ * Copyright (C) 2013 Jukka Svahn http://rahforum.biz
  * Licensed under GNU Genral Public License version 2
  * http://www.gnu.org/licenses/gpl-2.0.html
  */
